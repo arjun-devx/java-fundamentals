40128f055f631eb0a6507464f3a87e4323f2477c
Arjun Sharma <arjun.devx@gmail.com>
2025-06-01 01:04:18 +0530

    Data Hiding using the private and getter and setter - under the student class

--- java-fundamentals/_101_AccessModifier/src/oops/Student.java
+++ java-fundamentals/_101_AccessModifier/src/oops/Student.java
@@ -1,9 +1,12 @@
 package oops;
 
+import java.security.PrivateKey;
+
 public class Student {
     //Attributes
     public int id;
     public String name;
+    private String contactNo; //making contactNo private - data Hiding in Encapsulation, allow access only through getter and setter 
 
     //constructor overload
     //manual constructor overwriting the default constructor
@@ -29,5 +32,14 @@ public class Student {
     public void display() {
         System.out.println("id: " + id);
         System.out.println("name: " + name);
+    }
+
+    //getter and setter - here you can implement any auth check or match criteria to allow accessing the private contactNo attribute
+    public String getContactNo() {
+        return contactNo;
+    }
+
+    public void setContactNo(String contactNo) {
+        this.contactNo = contactNo;
     }
 }
