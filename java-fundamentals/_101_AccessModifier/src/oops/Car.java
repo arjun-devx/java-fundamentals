d98e660913393569c69012b24302523aa683cf18
Arjun Sharma <arjun.devx@gmail.com>
2025-06-01 00:47:30 +0530

    Constructor chaining concept code added in Car super class and ElectricCar child class

--- java-fundamentals/_101_AccessModifier/src/oops/Car.java
+++ java-fundamentals/_101_AccessModifier/src/oops/Car.java
@@ -5,6 +5,27 @@ public class Car {
     public String headLight;
     public String backLight;
 
+    //when I extended this super class CAR to other child as per the concept of the OOPS - inheritance with default constructor in this class -
+    // the child class were able to inherit the attributes provided from the super class without any error.
+    //But as soon as I added manual parameterized constructor - the child class started throwing errors. "There is no default constructor available in 'oops.Car'"
+    //parameterized constructor below
+
+    //Default Constructor
+    public Car() {
+        System.out.println("You are viewing print statement from the parent or super class constructor");
+    }
+
+    public  Car(int noOfWheels) {
+        this.noOfWheels = noOfWheels;
+    }
+
+    public Car(int noOfWheels, String headLight, String backLight) {
+        this.noOfWheels = noOfWheels;
+        this.headLight = headLight;
+        this.backLight = backLight;
+    }
+
+
     public void displayCar() {
         System.out.println("Attribute from the Super Class Car - 'No. Of Wheels in the Car noOfWheels': " + noOfWheels);
         System.out.println("Attribute from the Super Class Car - 'headLight of the Car headLight': " + headLight);
--- java-fundamentals/_101_AccessModifier/src/oops/ElectricCar.java
+++ java-fundamentals/_101_AccessModifier/src/oops/ElectricCar.java
@@ -2,4 +2,20 @@ package oops;
 
 public class ElectricCar extends Car {
     public int batteryCount;
+    public int batteryCapacity;
+
+    public ElectricCar() {
+        System.out.println("You are viewing the print statement from the ElectricCar sub class");
+    }
+
+    // If a parent class has only a parameterized constructor, then the child class constructor
+    // must call it using 'super(...)' to pass required values, because Java does not insert a default call to 'super()' automatically.
+
+    public ElectricCar(int noOfWheels, int batteryCount, int batteryCapacity) {
+        super(noOfWheels);
+        this.batteryCount = batteryCount;
+        this.batteryCapacity = batteryCapacity;
+    }
+
+
 }
