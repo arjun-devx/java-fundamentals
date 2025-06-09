b2a3c445a2b0757b804db22f9e1605a436df52f3
Arjun Sharma <arjun.devx@gmail.com>
2025-06-09 17:26:07 +0530

    ThreadPool Demo using Callable - FibonacciSeriesUsingFixedThread

--- java-fundamentals/ThreadDemo/src/ExecutorsAndCallables/FibonacciUsingFixedThread.java
+++ java-fundamentals/ThreadDemo/src/ExecutorsAndCallables/FibonacciUsingFixedThread.java
@@ -1,12 +1,30 @@
 package ExecutorsAndCallables;
 
-public class FibonacciUsingFixedThread {
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class FibonacciUsingFixedThread implements Callable<Integer> {
+
     private int number;
+
     public FibonacciUsingFixedThread (int n) {
         this.number = n;
     }
-    public int getSeries(int number) {
+
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("from thread: " + Thread.currentThread().getName());
         if (number <= 1) return number;
-        return getSeries(number-2) + getSeries(number-1);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        Future<Integer> futureAns1 = executorService.submit(new FibonacciUsingFixedThread(number-1));
+        Future<Integer> futureAns2 = executorService.submit(new FibonacciUsingFixedThread(number - 2));
+
+        int ans1 = futureAns1.get();
+        int ans2 = futureAns2.get();
+        return ans1 + ans2;
     }
 }
--- java-fundamentals/ThreadDemo/src/ExecutorsAndCallables/FibonacciUsingFixedThreadMain.java
+++ java-fundamentals/ThreadDemo/src/ExecutorsAndCallables/FibonacciUsingFixedThreadMain.java
@@ -1,9 +1,15 @@
 package ExecutorsAndCallables;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class FibonacciUsingFixedThreadMain {
-    public static void main(String args[]) {
-        FibonacciUsingFixedThread fibonacciUsingFixedThread = new FibonacciUsingFixedThread(5);
-        int result = fibonacciUsingFixedThread.getSeries(4);
+    public static void main(String args[]) throws Exception {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        Future<Integer> resultFuture = executorService.submit(new FibonacciUsingFixedThread(3));
+        int result = resultFuture.get();
         System.out.println(result);
     }
 }
