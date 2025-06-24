5e2162143d07441d3dbe93b734410cea2b3562b8
Arjun Sharma <arjun.devx@gmail.com>
2025-06-24 15:35:15 +0530

    Added example of FunctionalInterface

--- java-fundamentals/LLDDemo/src/advancedConcepts/Validator.java
+++ java-fundamentals/LLDDemo/src/advancedConcepts/Validator.java
@@ -1,5 +1,6 @@
 package advancedConcepts;
 
-public interface Validator {
+@FunctionalInterface // this annotation is just for readability purpose only, does not make any difference in execution
+public interface Validator { // interface which only implements single method are called functional Interface
      Boolean validate();
 }
