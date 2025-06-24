387f3534e76eb538573b0df8d25760f8aac5a68b
Arjun Sharma <arjun.devx@gmail.com>
2025-06-24 15:37:23 +0530

    Added example of abstract keyword in FunctionalInterface

--- java-fundamentals/LLDDemo/src/advancedConcepts/Validator.java
+++ java-fundamentals/LLDDemo/src/advancedConcepts/Validator.java
@@ -1,6 +1,6 @@
 package advancedConcepts;
 
 @FunctionalInterface // this annotation is just for readability purpose only, does not make any difference in execution
-public interface Validator { // interface which only implements single method are called functional Interface
-     Boolean validate();
+public interface Validator { // interface which only implements single abstrace method are called functional Interface
+     abstract Boolean validate(); // abstract keyword is greyed out - either you can put it there or dont put it - doesnt impact the execution of the code
 }
